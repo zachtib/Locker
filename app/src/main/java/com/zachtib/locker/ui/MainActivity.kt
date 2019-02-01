f9213e7f378e55704a8ecfeaf704ddd1ad1407da
Zach Tibbitts <zachtib@gmail.com>
2019-01-31 19:24:13 -0500

    add MainActivity

---
+++ app/src/main/java/com/zachtib/locker/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.zachtib.locker.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zachtib.locker.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
