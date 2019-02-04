6cdeda14414bc129e4ac0cef0de5af244ec05f14
Zach Tibbitts <zachtib@gmail.com>
2019-02-04 16:01:55 -0500

    add support for menus

--- app/src/main/java/com/zachtib/locker/framework/ui/FragmentView.kt
+++ app/src/main/java/com/zachtib/locker/framework/ui/FragmentView.kt
@@ -1,20 +1,31 @@
 package com.zachtib.locker.framework.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.annotation.LayoutRes
+import androidx.annotation.MenuRes
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-abstract class FragmentView(@LayoutRes private val layoutId: Int) : Fragment(), CoroutineScope {
+abstract class FragmentView(
+    @LayoutRes private val layoutId: Int,
+    @MenuRes private val menuId: Int? = null
+) : Fragment(), CoroutineScope {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(layoutId, container, false)
+        val view = inflater.inflate(layoutId, container, false)
+        setHasOptionsMenu(menuId != null)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        if (menuId != null) {
+            inflater?.inflate(menuId, menu)
+        }
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     private val job = Job()
