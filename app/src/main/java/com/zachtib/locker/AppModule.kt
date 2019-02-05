81bc2b03dadc16665b0a17b6486ac9987b6b3771
Zach Tibbitts <zachtib@gmail.com>
2019-02-05 08:33:45 -0500

    di for viewmodel

--- app/src/main/java/com/zachtib/locker/AppModule.kt
+++ app/src/main/java/com/zachtib/locker/AppModule.kt
@@ -1,7 +1,12 @@
 package com.zachtib.locker
 
+import com.zachtib.locker.data.LockerDatabase
+import com.zachtib.locker.ui.notes.list.NotesListViewModel
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val appModule = module {
-//    single { LockerDatabase.build(get())}
+    single { LockerDatabase.build(get())}
+
+    viewModel { NotesListViewModel(get()) }
 }
