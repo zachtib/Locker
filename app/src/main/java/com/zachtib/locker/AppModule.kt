434dd59974b46968e25d40dc73c07a9c57d50b8a
Zach Tibbitts <zachtib@gmail.com>
2019-02-09 18:22:22 -0500

    create notes

--- app/src/main/java/com/zachtib/locker/AppModule.kt
+++ app/src/main/java/com/zachtib/locker/AppModule.kt
@@ -3,20 +3,19 @@ package com.zachtib.locker
 import com.zachtib.locker.data.LockerDatabase
 import com.zachtib.locker.data.NotesRepository
 import com.zachtib.locker.ui.notes.detail.NoteDetailViewModel
+import com.zachtib.locker.ui.notes.editor.NoteEditorViewModel
 import com.zachtib.locker.ui.notes.list.NotesListViewModel
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val appModule = module {
-    single { LockerDatabase.build(get())}
+    single { LockerDatabase.build(get()) }
 
-    single {
-        val db: LockerDatabase = get()
-        db.notesDao()
-    }
+    single { get<LockerDatabase>().notesDao() }
 
     single { NotesRepository(get()) }
 
     viewModel { NotesListViewModel(get()) }
     viewModel { NoteDetailViewModel(get()) }
+    viewModel { NoteEditorViewModel(get()) }
 }
